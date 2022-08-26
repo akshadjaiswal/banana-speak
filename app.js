5e9dd86ced47fad01a47082bf3356c46a6bfa559
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2022-08-26 23:04:55 +0530

    translation

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 var btnTranslate = document.querySelector("#btn-translate");
-console.log(btnTranslate);
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(input) {
+  return serverURL + "?" + "text=" + input;
+}
+function errorHandler(error) {
+  console.log("error occured" + text);
+  alert("something wrong with server ! Try again after some time");
+}
+
+function clickHandler() {
+  // outputDiv.innerText ="jsjkdbsjjniub "+ txtInput.value;
+  var inputText = txtInput.value; //taking input
+
+  //calling server for processing
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+btnTranslate.addEventListener("click", clickHandler);
